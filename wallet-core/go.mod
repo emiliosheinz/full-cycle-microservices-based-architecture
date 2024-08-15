9dfa6cf1d0a388f914755887d23718aaf7427bf9
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-08-15 08:38:31 -0300

    feat: client db

--- wallet-core/go.mod
+++ wallet-core/go.mod
@@ -4,6 +4,7 @@ go 1.21.3
 
 require (
 	github.com/google/uuid v1.6.0
+	github.com/mattn/go-sqlite3 v1.14.22
 	github.com/stretchr/testify v1.9.0
 )
 
---
+++ wallet-core/internal/database/client_db_test.go
@@ -0,0 +1,53 @@
+package database
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/emiliosheinz/fc-ms-wallet-core/internal/entity"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/suite"
+)
+
+type ClientDBTestSuite struct {
+	suite.Suite
+	db       *sql.DB
+	clientDB *ClientDB
+}
+
+func (s *ClientDBTestSuite) SetupSuite() {
+	db, err := sql.Open("sqlite3", ":memory:")
+	s.Nil(err)
+	s.db = db
+	db.Exec("Create table clients (id varchar(255), name varchar(255), email varchar(255), created_at date)")
+	s.clientDB = NewClientDB(db)
+}
+
+func (s *ClientDBTestSuite) TierDownSuite() {
+	defer s.db.Close()
+	s.db.Exec("DROP TABLE clients")
+}
+
+func TestClientDBTestSuite(t *testing.T) {
+	suite.Run(t, new(ClientDBTestSuite))
+}
+
+func (s *ClientDBTestSuite) TestSave() {
+	client := &entity.Client{
+		ID:    "1",
+		Name:  "John Doe",
+		Email: "john@doe.com",
+	}
+	err := s.clientDB.Save(client)
+	s.Nil(err)
+}
+
+func (s *ClientDBTestSuite) TestGet() {
+	client, _ := entity.NewClient("John Doe", "john@doe.com")
+	s.clientDB.Save(client)
+	clientDB, err := s.clientDB.Get(client.ID)
+	s.Nil(err)
+	s.Equal(client.ID, clientDB.ID)
+	s.Equal(client.Name, clientDB.Name)
+	s.Equal(client.Email, clientDB.Email)
+}
